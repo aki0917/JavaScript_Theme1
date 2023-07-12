ca683fd92c036378692093a8352c4b7809a51b66
aki <akihirohigasa@AKIHIROnoMacBook-Pro.local>
2023-07-12 20:44:12 +0900

    js課題1-4修正

--- issue4/index.js
+++ issue4/index.js
@@ -1,25 +1,25 @@
 //javaScript課題①-4
 
 const tasks = [
-  { name: '掃除', genre: '家事' },
-  { name: '買い物', genre: '外出' },
-  { name: '散歩', genre: '運動' }
+  { content: '掃除', genre: '家事' },
+  { content: '買い物', genre: '外出' },
+  { content: '散歩', genre: '運動' }
 ];
 
 // 最初のタスクを表示する
 tasks.forEach((task, index) => {
-  console.log(`${index} : [内容]${task.name}、[ジャンル]${task.genre}`);
+  console.log(`${index} : [内容]${task.content}、[ジャンル]${task.genre}`);
 });
 
-const newTaskName = prompt("タスクを入力してください：");
-const newTaskGenre = prompt("ジャンルを入力してください：");
+const content = prompt("タスクを入力してください：");
+const genre = prompt("ジャンルを入力してください：");
 
-if (newTaskName && newTaskGenre) { 
-  tasks.push({ name: newTaskName, genre: newTaskGenre });
+if (content && genre) { 
+  tasks.push({ content, genre });
   alert("新しいタスクを追加しました。");
 }
 
 // 更新されたタスクを表示する
 tasks.forEach((task, index) => {
-  console.log(`${index} : [内容]${task.name}、[ジャンル]${task.genre}`);
+  console.log(`${index} : [内容]${task.content}、[ジャンル]${task.genre}`);
 });
